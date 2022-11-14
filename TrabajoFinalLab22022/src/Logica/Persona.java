184c914563a74fb26e3fc496ad7474d2f8a0e8c7
Juan I <111068326+ruedajuan@users.noreply.github.com>
2022-11-14 15:44:51 -0300

    Update Persona.java
    clase abstracta persona

--- TrabajoFinalLab22022/src/Logica/Persona.java
+++ TrabajoFinalLab22022/src/Logica/Persona.java
@@ -1,4 +1,50 @@
 package Logica;
 
-public class Persona {
+import java.io.Serializable;
+
+public abstract class Persona implements Serializable {
+    private String nombre;
+    private String apellido;
+    private String altura;
+
+    public Persona(){};
+
+    public Persona(String nombre,String apellido, String altura){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.altura=altura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getAltura() {
+        return altura;
+    }
+
+    public void setAltura(String altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", altura='" + altura + '\'' +
+                '}';
+    }
 }
