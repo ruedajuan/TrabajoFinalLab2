27ab5c1af8bccffb102c82f457d1d57f3c85f121
soriajoan <109100570+soriajoan@users.noreply.github.com>
2022-11-14 15:45:29 -0300

    Update Equipos.java

--- TrabajoFinalLab22022/src/Logica/Equipos.java
+++ TrabajoFinalLab22022/src/Logica/Equipos.java
@@ -1,4 +1,63 @@
 package Logica;
 
-public class Equipos {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Equipos implements Informacion, Serializable {
+    private String nombre;
+    private String ciudad;
+    private int codigo;
+    private List<Jugadores> plantilla = new ArrayList<Jugadores>();
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public List<Jugadores> getPlantilla(){
+        return plantilla;
+    }
+    public void setPlantilla(List<Jugadores>plantilla){
+        this.plantilla=plantilla;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+
+
+    public Equipos (){
+    }
+    public Equipos(String nombre, String ciudad, int codigo){
+        this.nombre=nombre;
+        this.ciudad=ciudad;
+        this.codigo=codigo;
+    }
+    public void agregarJugadores(Jugadores jugador){
+        plantilla.add(jugador);
+    }
+    public void eliminarJugadores(String apellidoABorrar){
+        for(int i=0;i<plantilla.size();i++){
+            if(plantilla.get(i).getApellido().equals(apellidoABorrar)){
+                plantilla.remove(i);
+            }
+        }
+    }
+
+
+    @Override
+    public String verPlantilla() {
+        for (Jugadores jugador:plantilla){
+        System.out.println(jugador.toString());
+        }
+        return null;
+    }
+
+
 }
