aaab95c7f906b9224c0c7e126f733dec7f2ffb81
Juan I <111068326+ruedajuan@users.noreply.github.com>
2022-11-14 15:43:49 -0300

    Update Jugadores.java
    se cargo la clases Jugadores, con sus atributos, getter, setters y constructor

--- TrabajoFinalLab22022/src/Logica/Jugadores.java
+++ TrabajoFinalLab22022/src/Logica/Jugadores.java
@@ -1,4 +1,34 @@
 package Logica;
 
-public class Jugadores {
+import java.io.Serializable;
+
+public class Jugadores extends Persona implements Serializable {
+    private int dorsal;
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(int dorsal) {
+        this.dorsal = dorsal;
+    }
+
+    public Jugadores(){};
+
+    public Jugadores(String nombre, String apellido, String altura, int dorsal){
+        super(nombre, apellido, altura);
+        this.dorsal=dorsal;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Jugadores{" +
+                "nombre='" + super.getNombre() + '\'' +
+                ", apellido='" + super.getApellido() + '\'' +
+                ", altura='" + super.getAltura() + '\'' +
+                ", dorsal=" + dorsal +
+                '}';
+    }
 }
+
